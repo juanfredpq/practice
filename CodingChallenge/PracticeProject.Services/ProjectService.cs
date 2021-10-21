113a33e85ef6aee61fe335e4862927f335bcf68c
juanfredpq <juanfredpq@yahoo.com>
2021-10-21 16:10:40 -0600

    Add files via upload

---
+++ CodingChallenge/PracticeProject.Services/ProjectService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PracticeProject.Core;
+using PracticeProject.Core.Models;
+using PracticeProject.Core.Services;
+
+namespace PracticeProject.Services
+{
+    public class ProjectService : IProjectService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProjectService(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+       
+        public async Task<IEnumerable<Project>> GetAllWithUser()
+        {
+            return await _unitOfWork.Projects
+                .GetAllWithUserAsync();
+        }
+
+        public async Task<Project> GetProjectById(int id)
+        {
+            return await _unitOfWork.Projects
+                .GetWithUserByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Project>> GetProjectsByUserId(int userId)
+        {
+            return await _unitOfWork.Projects
+                .GetAllWithUserByUserIdAsync(userId);
+        }
+
+      
+    }
+}
---
+++ CodingChallenge/PracticeProject.Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PracticeProject.Core;
+using PracticeProject.Core.Models;
+using PracticeProject.Core.Services;
+
+namespace PracticeProject.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public UserService(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+       
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            return await _unitOfWork.Users.GetAllAsync();
+        }
+
+        public async Task<User> GetUserById(int id)
+        {
+            return await _unitOfWork.Users.GetByIdAsync(id);
+        }
+        
+    }
+}
