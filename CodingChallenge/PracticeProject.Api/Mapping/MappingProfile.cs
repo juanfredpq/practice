7a4d149f06b07355e66276b99fd76cd79e3a0123
juanfredpq <juanfredpq@yahoo.com>
2021-10-21 16:08:37 -0600

    Add files via upload

---
+++ CodingChallenge/PracticeProject.Api/Mapping/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using PracticeProject.Api.Resources;
+using PracticeProject.Core.Models;
+
+namespace PracticeProject.Api.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to Resource
+            CreateMap<Project, ProjectResource>();
+            CreateMap<User, UserResource>();
+            
+            // Resource to Domain
+            CreateMap<ProjectResource, Project>();
+            CreateMap<SaveProjectResource, Project>();
+            CreateMap<UserResource, User>();
+            CreateMap<SaveUserResource, User>();
+        }
+    }
+}
